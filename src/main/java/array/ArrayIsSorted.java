d5d7492011177d5bfc1dfa15cab64c93163a4d6c
Lukas <142339568+lukasb1b@users.noreply.github.com>
2023-09-22 10:03:45 +0200

    Update ArrayIsSorted.java

--- src/main/java/array/ArrayIsSorted.java
+++ src/main/java/array/ArrayIsSorted.java
@@ -1,16 +1,12 @@
 package array;
 
-import java.util.Arrays;
-
 public class ArrayIsSorted {
     public static boolean isSorted(int[] arr){
-        for(int i=0;i< arr.length-1;i++){
-            if(arr[i]<arr[i+1]){
+        for(int i = 0; i < arr.length-1; i++){
+            if(arr[i] < arr[i+1]){
                 return false;
             }
         }
         return true;
     }
 }
-//30seconds algorithems
-//TODO
